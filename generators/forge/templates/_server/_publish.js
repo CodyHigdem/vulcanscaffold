a5c047a0ab25f305b804aaa4673ec629e40b4910
Cody <meanderingmonarch@gmail.com>
2016-01-19 12:06:15 -0600

    Bug Fix: Missing notFound template

--- generators/forge/templates/_server/_publish.js
+++
@@ -1,6 +0,0 @@
-//===== yeoman hook =====//
-//The above line is required for our yeoman generator and should not be changed.
-
-Meteor.publish('<%= lowerCase %>s', function (/* args */) {
-  return <%= name %>s.find();
-});
