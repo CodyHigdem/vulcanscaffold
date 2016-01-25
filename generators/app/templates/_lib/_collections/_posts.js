71a07af68c504940d9311b75ecf10f999f395b53
Cody <meanderingmonarch@gmail.com>
2016-01-25 08:55:02 -0600

    Feature: added simple schema to posts.

--- generators/app/templates/_lib/_collections/_posts.js
+++ generators/app/templates/_lib/_collections/_posts.js
@@ -1,1 +1,18 @@
-//this is the posts
+//Posts for Blog
+Posts = new Mongo.Collection('posts');
+
+if (Meteor.isServer) {
+  Posts.allow({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+}
